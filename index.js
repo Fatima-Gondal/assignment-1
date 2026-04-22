ad291fb504707fe86017cf3c65f390e699ae3f09
Fatima-Gondal <fatimagondal0797@gmail.com>
2026-04-22 20:25:42 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+let name ="fatima"
+
+console.log(name);
+// Addition
+    let x = 5;
+    let y = 2;
+    let z = x + y;
+
+console.log("The result of 5+2 is:" , z);   
+// Multiplication
+    let a = 2;
+    let b = 5;
+    let c = a * b;
+
+console.log("The result of 5*2 is:" , c);   
+// Subtraction
+    let d = 5;
+    let e = 2;
+    let f = d - e;
+
+console.log("The result of 5-2 is:" , f);
+// Division
+    let g = 5;
+    let h = 2;
+    let i = 5/2;
+    
+console.log("The result of 5/2 is:", i);    
+
+
+
